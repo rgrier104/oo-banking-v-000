bff2959990c2e8d33cb3df8a8f7e4703fc86a01f
Rachel Grier <rgrier104@gmail.com>
2018-09-27 13:19:56 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
   end
 
   def execute_transaction
-    if self.valid?
+    if valid?
       sender.balance = sender.balance - amount
       receiver.balance = receiver.balance + amount
       @status = "complete"
