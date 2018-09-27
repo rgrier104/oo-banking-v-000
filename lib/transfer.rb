c08cae9ad8f958886a09b9434331b45a1e05c5e2
Rachel Grier <rgrier104@gmail.com>
2018-09-27 13:12:11 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,22 @@
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, transfer_amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = transfer_amount
+    @status = "pending"
+  end
+
+  def valid?
+  end
+
+  def execute_transaction
+
+  end
+
+  def reverse_transfer
+  end
+
 end
