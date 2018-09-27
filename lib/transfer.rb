67c099cf4026934692c4982a4501bf92d7408954
Rachel Grier <rgrier104@gmail.com>
2018-09-27 13:35:33 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,7 +23,6 @@ class Transfer
       receiver.balance = receiver.balance + amount
       @status = "complete"
     else
-      @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
   end
