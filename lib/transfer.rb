fbcffa760f2ee30b19d379f0aadb8d31e39f2cbf
Rachel Grier <rgrier104@gmail.com>
2018-09-27 13:16:02 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,6 +20,7 @@ class Transfer
   def execute_transaction
     sender.balance = sender.balance - amount
     receiver.balance = receiver.balance + amount
+    @status = complete
   end
 
   def reverse_transfer
